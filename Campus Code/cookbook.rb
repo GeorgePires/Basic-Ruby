4b9887fc73cf9ce0370bfba830f67b5ea808e573
GeorgePires <georgeasp.pi@gmail.com>
2023-02-12 20:49:10 -0300

    :sparkles: feat/ add cookbook

--- Campus Code/cookbook.rb
+++ Campus Code/cookbook.rb
@@ -1,6 +1,41 @@
-puts 'Bem-vindo ao Cookbook, sua rede social de receitas'
+puts "Bem-vindo ao Cookbook, sua rede social de receitas"
 
-puts 'Digite o nome da receita: '
-name = gets.chomp
+def menu
+  puts " ------------------------------------------------"
+  puts "[1] Cadastrar uma receita"
+  puts "[2] Ver todas ás receitas"
+  puts "[3] Sair"
+  puts " ------------------------------------------------"
 
-puts "Receita cadastrada com sucesso! => #{name}"
+  print "Escolha uma opção: "
+  return gets.to_i
+end
+
+opcao = menu()
+
+receitas = []
+while (opcao != 3)
+  if (opcao == 1)
+    puts "Digite o nome da receita: "
+    nome = gets.chomp
+    
+    puts "Digite o tipo da receita: "
+    tipo = gets.chomp
+
+    receitas << { nome: nome, tipo: tipo }
+    puts "Receita #{nome} cadastrada com sucesso!"
+    puts
+
+  elsif (opcao == 2)
+    puts '=============== Receitas cadastradas =================='
+    receitas.each do |receita|
+     puts "#{receita[:nome]} - #{receita[:tipo]}" 
+    end
+  else
+    puts 'Opção inválida :('
+  end
+
+  opcao = menu()
+end
+
+puts 'Obrigado por usar o CookBook :)'
